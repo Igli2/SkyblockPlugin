4455baf6737f11a645950f3e35058f27b9720c6e
Igli2 <sg.gerdon@gmx.de>
2021-03-27 22:51:08 +0100

    changed unbreakable tools to more readable code

--- skyblock/src/main/java/skyblock/listeners/PrepareItemCraftListener.java
+++ skyblock/src/main/java/skyblock/listeners/PrepareItemCraftListener.java
@@ -1,8 +1,5 @@
 package skyblock.listeners;
 
-import java.util.Arrays;
-
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
@@ -12,10 +9,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import skyblock.SkyblockMain;
 
 public class PrepareItemCraftListener implements Listener {
-    public static final Material ALL_TOOLS[] = new Material[] { Material.CARROT_ON_A_STICK,
-            Material.WARPED_FUNGUS_ON_A_STICK, Material.FLINT_AND_STEEL, Material.WOODEN_SHOVEL,
-            Material.WOODEN_PICKAXE, Material.WOODEN_AXE, Material.WOODEN_HOE };
-
     SkyblockMain plugin;
 
     public PrepareItemCraftListener(SkyblockMain plugin) {
@@ -26,13 +19,12 @@ public class PrepareItemCraftListener implements Listener {
     public void prepareItemCraftEvent(PrepareItemCraftEvent event) {
         if (event.getRecipe() != null && event.getRecipe().getResult() != null) {
             ItemStack result = event.getRecipe().getResult();
-            for (Material m : PrepareItemCraftListener.ALL_TOOLS) {
-                if (m == result.getType()) {
-                    ItemMeta itemMeta = result.getItemMeta();
-                    itemMeta.setUnbreakable(true);
-                    result.setItemMeta(itemMeta);
-                    event.getInventory().setResult(result);
-                }
+            this.plugin.getLogger().info(String.valueOf(result.getType().getMaxDurability()));
+            if (result.getType().getMaxDurability() > 0) {
+                ItemMeta itemMeta = result.getItemMeta();
+                itemMeta.setUnbreakable(true);
+                result.setItemMeta(itemMeta);
+                event.getInventory().setResult(result);
             }
         }
     }
